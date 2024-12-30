de70f88573960acfc05d56c401edfdfca7ee91db
olya shymanska <olyashymanska3@gmail.com>
2024-12-29 22:53:58 -0600

    task-1 done

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+const categoriesList = document.querySelector('#categories');
+const categories = categoriesList.querySelectorAll('.item');
+console.dir(categories);
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+    const title = category.querySelector('h2').textContent;
+
+    console.log(`Category:${title}`);
+
+    const elements = category.querySelectorAll('ul li').length;
+
+    console.log(`Elements:${elements}`);
+});
