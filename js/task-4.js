ae5850e77796dcc21847d3229341481d0ff5df5e
olya shymanska <olyashymanska3@gmail.com>
2025-01-05 15:37:53 -0600

    task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (!formEl.elements.email.value.trim() || !formEl.elements.password.value.trim()) {
+        return alert("All form fields must be filled in");
+    };
+    
+    const formData = {
+        [formEl.elements.email.name]: formEl.elements.email.value.trim(),
+        [formEl.elements.password.name]: formEl.elements.password.value.trim(),
+    };
+
+    console.log(formData);
+
+    formEl.reset();
+});
