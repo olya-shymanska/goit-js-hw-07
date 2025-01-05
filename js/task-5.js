139a9aaee2dcb7aac04ebbdeecfd4ba89c9c8ffd
olya shymanska <olyashymanska3@gmail.com>
2025-01-05 16:27:58 -0600

    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  spanEl.textContent = newColor;
+});
